b73714ee40699e60cc09e6f5af7b46f7b6c1e14e
FoxPrime-y <yf17537413229@163.com>
2024-12-21 00:45:08 +0800

    first commit

---
+++ Fox/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,3 @@
+import comp from "D:/FoxPrime/Fox/.vuepress/.temp/pages/index.html.vue"
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
