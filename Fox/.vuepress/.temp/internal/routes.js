ae90c5897ec55d41f4fbaebaaec042bea0074eaa
FoxPrime-y <yf17537413229@163.com>
2024-12-21 02:03:03 +0800

    ash

--- Fox/.vuepress/.temp/internal/routes.js
+++ Fox/.vuepress/.temp/internal/routes.js
@@ -2,6 +2,5 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/GitHub/foxprime.github.io/Fox/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
-  ["/docs/", { loader: () => import(/* webpackChunkName: "docs_index.html" */"D:/GitHub/foxprime.github.io/Fox/.vuepress/.temp/pages/docs/index.html.js"), meta: {"title":""} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/GitHub/foxprime.github.io/Fox/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
