ab05976e6cfc674ceaf1b2580b93be10e27d0808
FoxPrime-y <yf17537413229@163.com>
2024-12-21 01:55:43 +0800

    化

--- Fox/.vuepress/.temp/pages/docs/index.html.js
+++ Fox/.vuepress/.temp/pages/docs/index.html.js
@@ -1,3 +1,3 @@
 import comp from "D:/GitHub/foxprime.github.io/Fox/.vuepress/.temp/pages/docs/index.html.vue"
-const data = JSON.parse("{\"path\":\"/docs/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1734714837000,\"contributors\":[{\"name\":\"FoxPrime-y\",\"username\":\"FoxPrime-y\",\"email\":\"yf17537413229@163.com\",\"commits\":1,\"url\":\"https://github.com/FoxPrime-y\"}]},\"filePathRelative\":\"docs/README.md\"}")
+const data = JSON.parse("{\"path\":\"/docs/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1734716559000,\"contributors\":[{\"name\":\"FoxPrime-y\",\"username\":\"FoxPrime-y\",\"email\":\"yf17537413229@163.com\",\"commits\":3,\"url\":\"https://github.com/FoxPrime-y\"}]},\"filePathRelative\":\"docs/README.md\"}")
 export { comp, data }
--- Fox/.vuepress/config.js
+++ Fox/.vuepress/config.js
@@ -3,6 +3,10 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  title: "FoxPrime",
+  dest: "docs", 
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+
+  }),
 })
